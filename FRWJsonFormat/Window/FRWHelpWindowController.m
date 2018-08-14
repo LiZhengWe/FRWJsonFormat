c7669532e7b0057fe8f7991202f0c57d3392edfc
yiner <yiner@2dfire.com>
2018-08-14 16:57:22 +0800

    feat：帮助 弹窗关闭问题修复

--- FRWJsonFormat/Window/FRWHelpWindowController.m
+++ FRWJsonFormat/Window/FRWHelpWindowController.m
@@ -18,6 +18,18 @@
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    
+    [[self.window standardWindowButton:NSWindowZoomButton] setHidden:YES];
+    [[self.window standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+    [[self.window standardWindowButton:NSWindowCloseButton] setTarget:self];
+    [[self.window standardWindowButton:NSWindowCloseButton] setAction:@selector(close)];
+}
+
+
+- (void)close {
+    [super close];
+    [NSApp stopModal];
+    [NSApp stop:self];
 }
 
 @end
